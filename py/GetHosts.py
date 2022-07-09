dd085ccb57738570710f424c96f8c5ade6f11e5d
无绘 <nineto0@163.com>
2022-07-09 18:05:10 +0800

    Update GetHosts.py

--- py/GetHosts.py
+++ py/GetHosts.py
@@ -1,11 +1,7 @@
 # -*- coding: utf-8 -*-
-from urllib import request
-import os,sys
 import socket
 hosts = ''
-dpath = os.path.abspath(os.path.join(os.getcwd(), ".."))
-print("当前目录 "+dpath)
-rfile = open(dpath+"/GithubHost/other/domain.txt","r")
+rfile = open("/GithubHost/other/domain.txt","r")
 domain = rfile.read().splitlines()
 print(domain)
 rfile.close()
@@ -17,6 +13,6 @@ for index in range(len(domain)):
       ip = "unknow"
       print("解析失败" + domain[index])
     hosts = hosts + ip + " " + domain[index]+"\r\n"
-wfile = open(dpath+"/GithubHost/hosts.txt",'w')
+wfile = open("/GithubHost/hosts.txt",'w')
 wfile.write(hosts)
 wfile.close()
